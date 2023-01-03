51492f0e92574a313fb5e3cf498211d40b63e251
bigstar One <bigstarone96@gmail.com>
2023-01-04 00:15:21 +0900

    x 변수에 들어 있는 값 출력 및 메모리 주소(16진수) 출력

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -2,13 +2,9 @@
 
 int main()
 {
-	std::cout << "숫자를 입력하고 엔터를 누르세요!" << std::endl;
+	int x;
+	x = 10;
 
-	int number;
-
-	std::cin >> number;
-	std::cout << "입력하신 숫자는 ";
-	std::cout << number;
-	std::cout << "입니다!";
+	std::cout << x << " : " << &x << std::endl;
 	
 }
