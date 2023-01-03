290f22de4b94932cd1f35cd936e417a0ac72fcff
bigstar One <bigstarone96@gmail.com>
2023-01-03 23:52:48 +0900

    입력한 숫자를 출력하는 프로그램

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+	std::cout << "숫자를 입력하고 엔터를 누르세요!" << std::endl;
+
+	int number;
+
+	std::cin >> number;
+	std::cout << "입력하신 숫자는 ";
+	std::cout << number;
+	std::cout << "입니다!";
 	
 }
