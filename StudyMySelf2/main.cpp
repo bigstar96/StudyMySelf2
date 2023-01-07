1fb5ddd1627b48c065de6d464c0ea6ac5e148123
bigstar One <bigstarone96@gmail.com>
2023-01-08 03:39:32 +0900

    연습문제 - 지하철 요금 계산

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,59 +1,158 @@
 #include <iostream>
 
-void OddEvenNumber()
+void Subway()
 {
-	int number{};
+	int age;
+	int payment;
+	int distance;
+	std::cout << "나이를 입력하세요 : ";
+	std::cin >> age;
+	std::cout << "결제 수단을 입력하세요 { 0 : 교통카드, 1 : 일반(현금) }";
+	std::cin >> payment;
+	std::cout << "거리를 입력하세요 : ";
+	std::cin >> distance;
 
-	std::cout << "숫자를 입력하세요 : ";
-	std::cin >> number;
-
-	std::cout << "입력하신 " << number << "은(는)";
-	if (number % 2)
+	if (payment == 0)
 	{
-		std::cout << "홀수 입니다.";
+		if (distance < 10)
+		{
+			if (age < 6)
+			{
+				std::cout << "무료";
+			}
+			else if (age >= 6 && age < 13)
+			{
+				std::cout << "450원";
+			}
+			else if (age >= 13 && age < 18)
+			{
+				std::cout << "720원";
+			}
+			else
+			{
+				std::cout << "1,250원";
+			}
+		}
+
+		else if (distance < 15)
+		{
+			if (age < 6)
+			{
+				std::cout << "무료";
+			}
+			else if (age >= 6 && age < 13)
+			{
+				std::cout << "500원";
+			}
+			else if (age >= 13 && age < 18)
+			{
+				std::cout << "800원";
+			}
+			else
+			{
+				std::cout << "1,350원";
+			}
+		}
+
+		else if (distance < 20)
+		{
+			if (age < 6)
+			{
+				std::cout << "무료";
+			}
+			else if (age >= 6 && age < 13)
+			{
+				std::cout << "550원";
+			}
+			else if (age >= 13 && age < 18)
+			{
+				std::cout << "880원";
+			}
+			else
+			{
+				std::cout << "1,450원";
+			}
+		}
+		else
+		{
+			std::cout << "잘못된 거리입니다.";
+		}
+	}
+	else if (payment == 1)
+	{
+		if (distance < 10)
+		{
+			if (age < 6)
+			{
+				std::cout << "무료";
+			}
+			else if (age < 13)
+			{
+				std::cout << "450원";
+			}
+			else if (age >= 13)
+			{
+				std::cout << "1,350원";
+			}
+			else
+			{
+				std::cout << "잘못 입력하셨습니다.";
+			}
+
+		}
+
+		else if (distance < 15)
+		{
+			if (age < 6)
+			{
+				std::cout << "무료";
+			}
+			else if (age < 13)
+			{
+				std::cout << "500원";
+			}
+			else if (age >= 13)
+			{
+				std::cout << "1,450원";
+			}
+			else
+			{
+				std::cout << "잘못 입력하셨습니다.";
+			}
+
+		}
+
+		else if (distance < 20)
+		{
+			if (age < 6)
+			{
+				std::cout << "무료";
+			}
+			else if (age < 13)
+			{
+				std::cout << "550원";
+			}
+			else if (age >= 13)
+			{
+				std::cout << "1,550원";
+			}
+			else
+			{
+				std::cout << "잘못 입력하셨습니다.";
+			}
+		}
+		else
+		{
+			std::cout << "잘못된 거리입니다.";
+		}
 	}
 	else
 	{
-		std::cout << "짝수 입니다.";
-	}
-}
-
-void Dollar()
-{
-	const int USDTOWON = 1260;
-	int dollar{};
-
-	std::cout << "USD를 입력하세요 : ";
-	std::cin >> dollar;
-
-	std::cout << dollar << " USD는 ";
-	std::cout << dollar * USDTOWON << "원 입니다.";
-}
-
-void YenOrDollar()
-{
-	const int USDTOWON = 1260;
-	const int JPYTOWON = 954;
-
-	int money{};
-	char kind;
-
-	std::cout << "달러(S)화 혹은 엔(Y)화를 입력하세요 : ";
-	std::cin >> money >> kind;
-
-	if (kind == 'S' || kind == 's')
-	{
-		std::cout << money << " USD는 ";
-		std::cout << money * USDTOWON << "원 입니다.";
-	}
-	else if (kind == 'Y' || kind == 'y')
-	{
-		std::cout << money << " JPY는 ";
-		std::cout << money * JPYTOWON << "원 입니다.";
+		std::cout << "잘못된 결제 수단입니다.";
 	}
 }
 
 int main()
 {
-	YenOrDollar();
+	Subway();
 }
