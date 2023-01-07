4ab6d9b92712735082ac033c14b6fd3a83d81018
bigstar One <bigstarone96@gmail.com>
2023-01-08 03:09:11 +0900

    3항 연산자 - 음수를 양수로 바꾸는 식

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -4,22 +4,9 @@
 
 int main()
 {
-	int alpha{ 255 }, red{ 255 }, green{ 0 }, blue{ 0 };
+	int inputNumber{};
+	std::cin >> inputNumber;
 
-	unsigned int color;
-
-	color = alpha << 24 | red << 16 | green << 8 | blue;
-
-	std::cout << std::hex;
-	std::cout << color << std::endl;
-
-	alpha = color >> 24;
-	red = color >> 16 & 0xFF;
-	green = color >> 8 & 0xFF;
-	blue = color & 0xFF;
-
-	std::cout << alpha << ", "
-		<< red << ", "
-		<< green << ", "
-		<< blue << std::endl;
+	int absoulteNumber = inputNumber < 0 ? -inputNumber : inputNumber;
+	std::cout << absoulteNumber << std::endl;
 }
