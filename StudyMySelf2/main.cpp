4524e3a3754ecda7d1f9cff4d0c4e13054774ac3
bigstar One <bigstarone96@gmail.com>
2023-01-11 04:45:16 +0900

    실습 - 환율 예제 유로(E)와 파운드(P) 추가

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -3,24 +3,41 @@
 
 int main()
 {
-	std::cout << "C++의 비트 연산자를 입력하세요.";
-	char inputOperator;
-	std::cin >> inputOperator;
+	const int USDTOWON = 1240;
+	const float YENTOWON = 9.46f;
+	const float EUROTOWON = 1340.85f;
+	const float POUNDTOWON = 1518.07f;
+	int money{};
+	char kind;
 
-	switch (inputOperator)
+	std::cout << "달러(S)화, 엔(Y)화, 유로(E)화, 파운드(P)화를 입력하세요 : ";
+	std::cin >> money >> kind;
+
+	switch (kind)
 	{
-	case '!':
-		std::cout << "단항 연산자";
-		break;
+		case 'S': case 's':
+			std::cout << money << " USD는 ";
+			std::cout << money * USDTOWON << "원 입니다.";
+			break;
 
-	case '&':
-	case '|':
-	case '^':
-		std::cout << "이항 연산자";
-		break;
+		case 'Y': case 'y':
+			std::cout << money << " JPY는 ";
+			std::cout << money * YENTOWON << "원 입니다.";
+			break;
 
-	default:
-		std::cout << "비트 연산자가 아닙니다.";
-		break;
+		case 'E': case 'e':
+			std::cout << money << " EUR는 ";
+			std::cout << money * EUROTOWON << "원 입니다.";
+			break;
+
+		case 'P': case 'p':
+			std::cout << money << " GBP는 ";
+			std::cout << money * POUNDTOWON << "원 입니다.";
+			break;
+
+		default:
+			std::cout << "알 수 없는 화폐 단위입니다.";
+			break;
 	}
+	
 }
