bdf27dde907cffb161908343de79f78dde60bdc8
bigstar One <bigstarone96@gmail.com>
2023-01-11 04:22:17 +0900

    실습 - 비트 연산자를 입력 받아 단항인지 이항인지 출력하기

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
 
-void BMI()
-{
-	double height{};
-	double weight{};
-	double bmi{};
-
-	std::cout << "키(cm)를 입력하세요 : ";
-	std::cin >> height;
-	std::cout << "체중(kg)를 입력하세요 : ";
-	std::cin >> weight;
-
-	// bmi = 몸무게 / (키 * 키)
-	// 몸무게(kg),키(m)
-
-	height /= 100;
-	bmi = weight / (height * height);
-
-	std::cout << "키 : " << height << ", 몸무게 : " << weight << "에 대한 BMI : " << bmi << std::endl;
-
-	if (bmi <= 18.5)
-	{
-		std::cout << "저체중입니다." << std::endl;
-	}
-	else if (bmi < 25)
-	{
-		std::cout << "정상입니다." << std::endl;
-	}
-	else if (bmi < 30)
-	{
-		std::cout << "과체중입니다." << std::endl;
-	}
-	else
-	{
-		std::cout << "비정상입니다." << std::endl;
-	}
-}
 
 int main()
 {
-	BMI();
+	std::cout << "C++의 비트 연산자를 입력하세요.";
+	char inputOperator;
+	std::cin >> inputOperator;
+
+	switch (inputOperator)
+	{
+	case '!':
+		std::cout << "단항 연산자";
+		break;
+
+	case '&':
+	case '|':
+	case '^':
+		std::cout << "이항 연산자";
+		break;
+
+	default:
+		std::cout << "비트 연산자가 아닙니다.";
+		break;
+	}
 }
