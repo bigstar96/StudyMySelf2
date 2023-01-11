9e85e6f06cd3a9fc01a66b61fe3773c0ce07949c
bigstar One <bigstarone96@gmail.com>
2023-01-12 02:06:01 +0900

    예제 - 이전 단어와 반복되는 단어가 입력되었는지 검사하는 프로그램

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,33 +1,21 @@
 #include <iostream>
 
-void DoWhile()
-{
-	int i{ 1 }, j{ 0 };
-	do
-	{
-		j = j + i;
-		std::cout << i++ << " , " << j << std::endl;
 
-	} while (i <= 10);
-}
-
-void While()
-{
-	int i{ 0 }, j{ 0 };
-
-	while (i < 10)
-	{
-		++i;
-		j += i;
-		std::cout << i << " , " << j << std::endl;
-	}
-}
 
 int main()
 {
-	// do while - 조건이 일치하지 않아도 한번은 실행
-	DoWhile();
+	std::string previous = " ";
+	std::string current;
 
-	// while - 조건이 맞지 않으면 한번도 실행하지 않음
-	While();
+	while (std::cin >> current)
+	{
+		if (previous == current)
+		{
+			std::cout << current << " is repeated word!" << std::endl;
+		}
+
+		previous = current;
+	}
+
+	return 0;
 }
