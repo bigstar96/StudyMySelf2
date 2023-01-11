efa2ec2aa7cb4cedc65533704d240c21ba1c5937
bigstar One <bigstarone96@gmail.com>
2023-01-12 04:26:47 +0900

    예제 - 세계 최초의 프로그램

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -4,24 +4,15 @@
 
 int main()
 {
-	std::string previous = " ";
-	std::string current;
-	int index{ 1 };
+	int count{ 0 };
 
-	while (std::cin >> current)
+	while (count < 100)
 	{
-		if (previous == current)
-		{
-			std::cout << index << "번째 단어 " << current << "이(가) 반복되었습니다." << std::endl;
-			index = 1;
-		}
-		else
-		{
-			++index;
-		}
-
-		previous = current;
+		std::cout << count << "\t" << count * count << std::endl;
+		count++;
 	}
+
+	std::cout << std::endl;
 
 	return 0;
 }
