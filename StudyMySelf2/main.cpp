7c28ae6926b506c88f55c1f13bae17bb03ce8bdc
bigstar One <bigstarone96@gmail.com>
2023-01-12 04:17:07 +0900

    연습 - 중복된 단어가 몇 번째 단어인지 출력

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -6,12 +6,18 @@ int main()
 {
 	std::string previous = " ";
 	std::string current;
+	int index{ 1 };
 
 	while (std::cin >> current)
 	{
 		if (previous == current)
 		{
-			std::cout << current << " is repeated word!" << std::endl;
+			std::cout << index << "번째 단어 " << current << "이(가) 반복되었습니다." << std::endl;
+			index = 1;
+		}
+		else
+		{
+			++index;
 		}
 
 		previous = current;
