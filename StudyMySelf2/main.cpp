bf20c654f8ed2c837929734b686841984428d57c
bigstar One <bigstarone96@gmail.com>
2023-01-15 02:03:18 +0900

    배열의 기초

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 
-union MyUnion
-{
-	int i;
-	float f;
-	char c;
-};
-
 
 
 int main()
 {
-	MyUnion myUnion;
+	int scores[10];
 
-	myUnion.i = 98;
-	std::cout << myUnion.i << ", " << myUnion.f << ", " << myUnion.c << std::endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << "Input score[" << i << "] : ";
+		std::cin >> scores[i];
+	}
 
-	myUnion.f = 1.1f;
-	std::cout << myUnion.i << ", " << myUnion.f << ", " << myUnion.c << std::endl;
-
-	myUnion.c = 'a';
-	std::cout << myUnion.i << ", " << myUnion.f << ", " << myUnion.c << std::endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << i << " : " << scores[i] << std::endl;
+	}
 }
