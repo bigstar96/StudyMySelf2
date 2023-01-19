9a7da395d869fbf7b7a3d1e1d5d606bd6e2cfd59
bigstar One <bigstarone96@gmail.com>
2023-01-19 21:19:34 +0900

    전역 변수 - 소스파일 어디에서도 사용 할 수 있는 변수 외부 변수 - 서로 다른 파일에 존재하는 변수 사용 (extern) 정적 변수 - 프로그램이 종료될 때까지 살아 남는 변수 (static)

---
+++ StudyMySelf2/file.cpp
@@ -0,0 +1,1 @@
+int myGlobalInteger = 5;
--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,17 +1,60 @@
 #include <iostream>
 
-int Square(int);
+// 전역 변수
+// int myGlobalInteger = 5;
 
-using namespace std;
+// 외부 변수
+extern int myGlobalInteger;
+
+void Step1()
+{
+	int myInteger{ 5 };
+
+	for (int i = 0; i < 5; ++i)
+	{
+		int myInteger2{ 0 };
+		myInteger = myInteger2 = i;
+	}
+	// std::cout << myInteger << " , " << myinteger2 << std::endl;
+}
+
+void Step2()
+{
+	int myInteger{};
+	myInteger = myGlobalInteger;
+
+	std::cout << myInteger << std::endl;
+}
+
+void func()
+{
+	int x;
+}
+void func2()
+{
+	int x;
+}
+
+void Step3()
+{
+	int myInteger{};
+	myInteger = myGlobalInteger;
+
+	std::cout << myInteger << std::endl;
+}
+
+// Step4
+int GetOrderNumber()
+{
+	static int number{ 0 };
+	return ++number;
+}
 
 int main()
 {
-	std::cout << Square(2) << std::endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << GetOrderNumber() << std::endl;
+	}
 
-	cout << pow(3, 4) << endl;
-}
-
-int Square(int x)
-{
-	return x * x;
 }
