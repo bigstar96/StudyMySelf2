75f40af1285e6e4080a9fde95afd2eb734c000a9
bigstar One <bigstarone96@gmail.com>
2023-01-19 22:39:37 +0900

    함수에 배열 넘겨주기 *배열은 매개변수로 처리될 때 그 방식이 다르다

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,60 +1,22 @@
-#include <iostream>
+#include <iostream>	
 
-// 전역 변수
-// int myGlobalInteger = 5;
-
-// 외부 변수
-extern int myGlobalInteger;
-
-void Step1()
+int Sum(int input[5])
 {
-	int myInteger{ 5 };
-
+	int sum{};
 	for (int i = 0; i < 5; ++i)
 	{
-		int myInteger2{ 0 };
-		myInteger = myInteger2 = i;
+		sum += input[i];
 	}
-	// std::cout << myInteger << " , " << myinteger2 << std::endl;
-}
 
-void Step2()
-{
-	int myInteger{};
-	myInteger = myGlobalInteger;
+	input[0] = 0;
+	input[1] = 0;
 
-	std::cout << myInteger << std::endl;
-}
-
-void func()
-{
-	int x;
-}
-void func2()
-{
-	int x;
-}
-
-void Step3()
-{
-	int myInteger{};
-	myInteger = myGlobalInteger;
-
-	std::cout << myInteger << std::endl;
-}
-
-// Step4
-int GetOrderNumber()
-{
-	static int number{ 0 };
-	return ++number;
+	return sum;
 }
 
 int main()
 {
-	for (int i = 0; i < 10; ++i)
-	{
-		std::cout << GetOrderNumber() << std::endl;
-	}
-
+	int numbers[] = { 1,2,3,4,5 };
+	std::cout << Sum(numbers);
+	std::cout << numbers[0] << " , " << numbers[1];
 }
