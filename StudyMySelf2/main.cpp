0e1db3d96bf8559e0d961ffac9f71f415f82348c
bigstar One <bigstarone96@gmail.com>
2023-01-21 19:24:30 +0900

    실습 - 그럴듯한 프로그램 만들기

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,24 +1,43 @@
-#include <iostream>
+#include <iostream>	
 
-char ConvertToUpperCase(char input);
+char ReadInput();
+void PrintHeader();
+void PrintTable(char start, char end);
+char GetAsciiCharacter(int code);
 
 int main()
 {
-	char buffer[50] = "Hello wolrd";
-	for (int i = 0; i < 50; ++i)
-	{
-		std::cout << ConvertToUpperCase(buffer[i]);
-	}
+	std::cout << "아스키 문자 시작, 끝을 입력하세요!";
+	char start{ ReadInput() }, end{ ReadInput()};
+
+	PrintHeader();
+
+	PrintTable(start, end);
 }
 
-char ConvertToUpperCase(char input)
+char ReadInput()
 {
-	if (input >= 'a' && input <= 'z')
+	char ch;
+	std::cin >> ch;
+	return ch;
+}
+
+void PrintHeader()
+{
+	std::cout << std::endl;
+	std::cout << "----- ASCII TABLE -----";
+	std::cout << std::endl;
+}
+
+char GetAsciiCharacter(int code)
+{
+	return (char)(code);
+}
+
+void PrintTable(char start, char end)
+{
+	for (int ch = start; ch <= end; ++ch)
 	{
-		return input - 'a' + 'A';
-	}
-	else
-	{
-		return input;
+		std::cout << ch << '\t' << GetAsciiCharacter(ch) << std::endl;
 	}
 }
