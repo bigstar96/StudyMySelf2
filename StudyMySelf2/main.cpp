8bbf93b2caa79535fa928453f184de7d58a32682
bigstar One <bigstarone96@gmail.com>
2023-01-22 21:30:30 +0900

    가변인자

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,43 +1,23 @@
-#include <iostream>	
+#include <iostream>
+#include <stdarg.h>
 
-char ReadInput();
-void PrintHeader();
-void PrintTable(char start, char end);
-char GetAsciiCharacter(int code);
+void function(int count, ...)
+{
+	va_list params;
+	va_start(params, count);
+	for (int i = 0; i < count; ++i)
+	{
+		std::cout << va_arg(params, int);
+	}
+	std::cout << std::endl;
+}
+
+
 
 int main()
 {
-	std::cout << "아스키 문자 시작, 끝을 입력하세요!";
-	char start{ ReadInput() }, end{ ReadInput()};
+	function(1, 1);
+	function(2, 2, 3);
 
-	PrintHeader();
-
-	PrintTable(start, end);
-}
-
-char ReadInput()
-{
-	char ch;
-	std::cin >> ch;
-	return ch;
-}
-
-void PrintHeader()
-{
-	std::cout << std::endl;
-	std::cout << "----- ASCII TABLE -----";
-	std::cout << std::endl;
-}
-
-char GetAsciiCharacter(int code)
-{
-	return (char)(code);
-}
-
-void PrintTable(char start, char end)
-{
-	for (int ch = start; ch <= end; ++ch)
-	{
-		std::cout << ch << '\t' << GetAsciiCharacter(ch) << std::endl;
-	}
+	return 0;
 }
