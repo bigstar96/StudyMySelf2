1f52f8ed3f8f1dbc49e5ef3846cfd2672946dc5a
bigstar One <bigstarone96@gmail.com>
2023-01-30 23:18:24 +0900

    연습문제 - 하노이 완성

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,46 +1,113 @@
 #include <iostream>
 
-int InputFromUser();
-void Move(char from, char to);
-void Hanoi(int n, char from, char temp, char to);
+bool Succes{ true };
+const int succes[3][3]{
+		{ 0, 0, 1 },
+		{ 0, 0, 2 },
+		{ 0, 0, 3 }
+};
 
+
+void PrintInfo(int hanoi[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << hanoi[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+	return;
+}
+
+void Move(int hanoi[3][3], int move1, int move2)
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		if (hanoi[i][move1] != 0)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (hanoi[j][move2] != 0)
+				{
+					if (hanoi[i][move1] < hanoi[j][move2])
+					{
+						hanoi[j - 1][move2] = hanoi[i][move1];
+						hanoi[i][move1] = 0;
+						return;
+					}
+					else
+					{
+						std::cout << "큰수를 작은수 위에 올릴수 없습니다." << std::endl;
+						return;
+					}
+				}
+				else if (hanoi[0][move2] == 0 &&
+					hanoi[1][move2] == 0 &&
+					hanoi[2][move2] == 0)
+				{
+					hanoi[2][move2] = hanoi[i][move1];
+					hanoi[i][move1] = 0;
+					return;
+				}
+			}
+		}
+	}
+}
+
+void Hanoi(int hanoi[3][3])
+{
+	int move1{ 0 }, move2{ 0 };
+
+	while (Succes)
+	{
+		PrintInfo(hanoi);
+		if (hanoi == succes)
+		{
+			std::cout << "정답입니다." << std::endl;
+			return;
+		}
+
+		std::cout << "어떻게 움직일까요? ";
+		std::cin >> move1 >> move2;
+
+		if (move1 == move2)
+		{
+			std::cout << "같은 곳에는 움직일수 없습니다." << std::endl;
+			continue;
+		}
+		else if (move1 < 0 || 3 < move1 ||
+			move2 < 0 || 3 < move2 ||
+			move1 == 0 || move2 == 0)
+		{
+			std::cout << "입력하신 숫자가 1~3 이 아닙니다." << std::endl;
+			continue;
+		}
+
+
+		Move(hanoi, move1 - 1, move2 - 1);
+
+		if (hanoi[0][2] == succes[0][2] &&
+			hanoi[0][1] == succes[0][1] &&
+			hanoi[0][0] == succes[0][0])
+		{
+			PrintInfo(hanoi);
+			std::cout << "정답입니다!" << std::endl;
+			Succes = false;
+		}
+	}
+}
 
 int main()
 {
-	int n = InputFromUser();
+	int hanoi[3][3]{
+		{ 1, 0, 0 },
+		{ 2, 0, 0 },
+		{ 3, 0, 0 }
+	};
 
-	Hanoi(n, 'A', 'B', 'C');
-}
+	Hanoi(hanoi);
 
-int InputFromUser()
-{
-	int n{};
-
-	std::cout << "<하노이의 탑>" << std::endl;
-	std::cout << "1. 한번에 하나의 원판만 옮길 수 있다." << std::endl;
-	std::cout << "2. 큰 원판이 작은 원판 위에 있어서는 안된다." << std::endl;
-	std::cout << "------------------------------" << std::endl;
-	std::cout << "원판의 갯수를 입려하세요 : ";
-	std::cin >> n;
-
-	return n;
-}
-
-void Move(int n, char from, char to)
-{
-	std::cout << from << "에서 " << to << "로 원반" << n << "을 옮깁니다." << std::endl;
-}
-
-void Hanoi(int n, char from, char temp, char to)
-{
-	if (n == 1)
-	{
-		Move(n, from, to);
-	}
-	else
-	{
-		Hanoi(n - 1, from, to, temp);
-		Move(n, from, to);
-		Hanoi(n - 1, temp, from, to);
-	}
+	return 0;
 }
