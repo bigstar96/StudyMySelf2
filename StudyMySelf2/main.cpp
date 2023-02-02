a494c435878a017dacb4d4d12f68c312c711ab5e
bigstar One <bigstarone96@gmail.com>
2023-02-02 22:32:45 +0900

    포인터활용

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,48 +1,70 @@
 #include <iostream>
 
-void Pointer()
+void swap(int* x, int* y)
 {
-	int a{ 1025 };
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
 
-	int* pa = &a;
+void PrintValue(const int& x)
+{
+	std::cout << x << std::endl;
+}
 
-	char* p0 = (char*)pa;
+int Sum(int x, int y)
+{
+	return x + y;
+}
 
-	for (int i = 0; i < 4; ++i)
+int Sigma(int (*f)(int x), int m, int n)
+{
+	int sum = 0;
+	for (int k = m; k <= n; ++k)
 	{
-		std::cout << (void*)p0 << " : " << (int)*p0 << std::endl;
-		p0++;
+		sum += f(k);
 	}
+
+	return sum;
 }
 
-void PointerOfAPointer()
+int NormalFunction(int x)
 {
-	int a{ 10 };
-	int* p = &a;
-	int** pp = &p;
+	return x;
 }
 
-void StructureAndPointer()
+int SquareSunction(int x)
 {
-	struct Status
-	{
-		int HP;
-		int MP;
-	};
-
-	Status myStatus{ 100,10 };
-
-	Status* pStatus = &myStatus;
-
-	std::cout << (*pStatus).HP << std::endl;
+	return x * x;
 }
 
 int main()
 {
-	const int x{ 10 }, y{ 20 };
-	const int* p;
-	p = &x;
-	*p = 1;
+	/*int x{ 10 }, y{ 20 };
+	swap(&x, &y);
+	std::cout << x << " , " << y << std::endl;*/
 
-	std::cout << *p << std::endl;
+	/*const int& ref3{ 10 + 20 };
+	std::cout << ref3 << std::endl;
+
+	int x1{ 10 };
+	PrintValue(x1);
+
+	const int y1{ 20 };
+	PrintValue(y1);
+	PrintValue(30);
+	PrintValue(40 + 50);*/
+
+	/*int x{ 1 }, y{ 2 };
+	std::cout << &x << " , " << &y << std::endl;
+	std::cout << Sum << " , " << main << std::endl;
+	std::cout << &Sum << " , " << &main << std::endl;
+
+	Sum(x, y);*/
+
+	std::cout << "NormalFunction : " << &NormalFunction << std::endl;
+	std::cout << Sigma(NormalFunction, 1, 10) << std::endl;
+	std::cout << Sigma(SquareSunction, 1, 10) << std::endl;
+
+
 }
