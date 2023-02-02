1272c40e0f965a5ead349c700d58e5ef24037c64
bigstar One <bigstarone96@gmail.com>
2023-02-02 22:47:42 +0900

    연습 - 오름차순, 내림차순 비교함수 추가

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -4,13 +4,27 @@
 // 함수 시그니쳐
 // void Sort(int numbers[], int count)
 
-void Sort(int numbers[], int count)
+// 오름차순, 내림차순 비교함수를 만든 후 함수 포인터로 확장하시오.
+
+using Comparison = bool (*)(int, int);
+
+bool Asscending(int x, int y)
+{
+	return x > y;
+}
+
+bool Descending(int x, int y)
+{
+	return x < y;
+}
+
+void Sort(int numbers[], int count, Comparison f)
 {
 	for (int i = 0; i < count; ++i)
 	{
 		for (int j = 0; j < i; ++j)
 		{
-			if (numbers[i] < numbers[j])
+			if (f(numbers[i], numbers[j]))
 			{
 				int temp = numbers[i];
 				numbers[i] = numbers[j];
@@ -24,9 +38,9 @@ void Sort(int numbers[], int count)
 int main()
 {
 	const int NumArray = 10;
-	int scores[NumArray]{ 20,10,40,15,30 };
+	int scores[NumArray]{ 20,10,40,15,30,70,85,100,60,90 };
 
-	Sort(scores, NumArray);
+	Sort(scores, NumArray, Descending);
 
 	for (int i = 0; i < NumArray; ++i)
 	{
