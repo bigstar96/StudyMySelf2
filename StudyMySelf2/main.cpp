c46c4c2dd9adf930288e40608aafe7780e42b3bb
bigstar One <bigstarone96@gmail.com>
2023-02-03 10:29:07 +0900

    배열포인트

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,50 +1,34 @@
 #include <iostream>
 
-// 실습 - 내림차순 정렬 문제를 다음과 같은 함수로 만들어 보세요.
-// 함수 시그니쳐
-// void Sort(int numbers[], int count)
 
-// 오름차순, 내림차순 비교함수를 만든 후 함수 포인터로 확장하시오.
-
-using Comparison = bool (*)(int, int);
-
-bool Asscending(int x, int y)
-{
-	return x > y;
-}
-
-bool Descending(int x, int y)
-{
-	return x < y;
-}
-
-void Sort(int numbers[], int count, Comparison f)
-{
-	for (int i = 0; i < count; ++i)
-	{
-		for (int j = 0; j < i; ++j)
-		{
-			if (f(numbers[i], numbers[j]))
-			{
-				int temp = numbers[i];
-				numbers[i] = numbers[j];
-				numbers[j] = temp;
-			}
-		}
-	}
-
-}
 
 int main()
 {
-	const int NumArray = 10;
-	int scores[NumArray]{ 20,10,40,15,30,70,85,100,60,90 };
+	int array[10]{ 0 };
 
-	Sort(scores, NumArray, Descending);
-
-	for (int i = 0; i < NumArray; ++i)
+	for (int i = 0; i < 10; ++i)
 	{
-		std::cout << scores[i];
-		if (i < NumArray - 1) std::cout << ", ";
+		std::cout << &array[i] << std::endl;
+	}
+
+	// 기존 방법
+	int sum1{};
+	for (int i = 9; i < 10; ++i)
+	{
+		sum1 += array[i];
+	}
+
+	// 포인터 활용
+	int sum2{};
+	for (int i = 0; i < 10; ++i)
+	{
+		sum2 += *(array + i);
+	}
+
+	// 포인터 및 for문 고급
+	int sum3{};
+	for (int i = 0, *p = array; i < 10; ++i, ++p)
+	{
+		sum3 += *p;
 	}
 }
