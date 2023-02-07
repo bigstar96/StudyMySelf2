8afeb51ee05d02b1a65d11581896963f16b9c2ea
bigstar One <bigstarone96@gmail.com>
2023-02-08 01:10:16 +0900

    연습문제 - 몇 명의 친구가 있는지 물어보고 가장 이름이 긴 친구의 이름을 출력하시오

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -4,9 +4,37 @@
 
 int main()
 {
-	int* pNumbers{ new int[] { 4,8,5,3,1} };
+	int SizePeople{};
+	std::cout << "몇 명의 친구가 있나요? ";
+	std::cin >> SizePeople;
+
+	if (SizePeople <= 0)
+	{
+		std::cout << "잘못된 크기입니다." << std::endl;
+		return 0;
+	}
+
+	std::string* pName = new std::string[SizePeople];
+
+	for (int i = 0; i < SizePeople; ++i)
+	{
+		std::cout << "친구 이름 #" << i + 1 << " : ";
+		std::cin >> pName[i];
+	}
+
+	int num{};
+	int maxlen{};
+	for (int i = 0; i < SizePeople; ++i)
+	{
+		if (pName[i].size() > maxlen)
+		{
+			maxlen = pName[i].size();
+			num = i;
+		}
+	}
+	
+	std::cout << "이름이 가장 긴 친구는 " << pName[num] << " 입니다." << std::endl;
 
 
-
-	delete pNumbers;
+	delete[] pName;
 }
