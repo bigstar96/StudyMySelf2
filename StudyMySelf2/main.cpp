7e5c46767c290b40c80cce170b229df829b259e2
bigstar One <bigstarone96@gmail.com>
2023-02-09 00:08:51 +0900

    C/C++ 언어의 Compile 원리

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -2,17 +2,25 @@
 
 
 
-int main(int argc, char* argv[])
+int main()
 {
-	// int argc - argument count의 약자로 매개변수 갯수를 지정합니다.
-	// char* argv[] - argument vector의 약자로 매개변수 목록입니다.
-	//	타입에서 힌트를 얻자면 char[]의 * 이므로 문자열의 포인터, 즉 문자열의 배열형태라는 것을 알 수 있습니다.
+	const int a;	// a is int which is constant
+					// a는 정수이며 상수형입니다.
+	int const b;	// b is constant int
+					// b는 상수형 정수입니다.
 
-	// 위 예제에서 사용한 명령을 기준으로 설명 하면 다음과 같다.
-	// argc - 3
-	// argv {
-	//			"-NoVSync",
-	//			"-FULLSCREEN",
-	//			"-FPS"
-	//		}
+	const int* pa;	// pa is pointer to int which is constant
+					// pa는 포인터로 정수를 가리키며 그 정수는 상수입니다.
+	int* const pb;	// pb is constant pointer to int
+					// pb는 상수 포인터로 정수를 가리킵니다.
+	int const* pc;	// pc is pointer to constant int
+					// pb는 상수 정수를 가리키는 포인터입니다.
+
+
+	*pa = 1;		// pa가 가리키는 정수는 상수이므로 값을 변경할 수 없습니다.
+	pa++;			// pa는 일반 포인터이므로 이동 가능합니다.
+	*pb = 1;		// pb는 일반 정수를 가리키므로 값을 변경할 수 있습니다.
+	pb++;			// pb는 상수 포인터이므로 이동 불가능합니다.
+	*pc = 1;		// pc는 상수 정수를 가리키므로 값을 변경할 수 없습니다.
+	pc++;			// pc는 일반 포인터이므로 이동 가능합니다.
 }
