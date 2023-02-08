e18043f930f78d728f54c083c6102d44d427b6c8
bigstar One <bigstarone96@gmail.com>
2023-02-08 23:39:25 +0900

    main() 함수

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -2,13 +2,17 @@
 
 
 
-int main()
+int main(int argc, char* argv[])
 {
-	int number = 5;
-	int* p = &number;
+	// int argc - argument count의 약자로 매개변수 갯수를 지정합니다.
+	// char* argv[] - argument vector의 약자로 매개변수 목록입니다.
+	//	타입에서 힌트를 얻자면 char[]의 * 이므로 문자열의 포인터, 즉 문자열의 배열형태라는 것을 알 수 있습니다.
 
-	std::cout << p << " : " << *p << std::endl;
-
-	int** pp = &p;
-	std::cout << pp << " : " << *pp << " : " << **pp;
+	// 위 예제에서 사용한 명령을 기준으로 설명 하면 다음과 같다.
+	// argc - 3
+	// argv {
+	//			"-NoVSync",
+	//			"-FULLSCREEN",
+	//			"-FPS"
+	//		}
 }
