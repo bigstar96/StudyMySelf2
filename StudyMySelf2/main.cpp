463cd0b512dc95e50da2bb05a6c9eea4c8645f18
bigstar One <bigstarone96@gmail.com>
2023-02-08 23:30:21 +0900

    동적 2차 배열

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -4,37 +4,11 @@
 
 int main()
 {
-	int SizePeople{};
-	std::cout << "몇 명의 친구가 있나요? ";
-	std::cin >> SizePeople;
+	int number = 5;
+	int* p = &number;
 
-	if (SizePeople <= 0)
-	{
-		std::cout << "잘못된 크기입니다." << std::endl;
-		return 0;
-	}
+	std::cout << p << " : " << *p << std::endl;
 
-	std::string* pName = new std::string[SizePeople];
-
-	for (int i = 0; i < SizePeople; ++i)
-	{
-		std::cout << "친구 이름 #" << i + 1 << " : ";
-		std::cin >> pName[i];
-	}
-
-	int num{};
-	int maxlen{};
-	for (int i = 0; i < SizePeople; ++i)
-	{
-		if (pName[i].size() > maxlen)
-		{
-			maxlen = pName[i].size();
-			num = i;
-		}
-	}
-	
-	std::cout << "이름이 가장 긴 친구는 " << pName[num] << " 입니다." << std::endl;
-
-
-	delete[] pName;
+	int** pp = &p;
+	std::cout << pp << " : " << *pp << " : " << **pp;
 }
