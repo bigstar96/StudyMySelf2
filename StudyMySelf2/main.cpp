5b3a5586e2a080f2c8a06b8c932582ef09b32ae9
bigstar One <bigstarone96@gmail.com>
2023-02-10 02:48:05 +0900

    예외처리

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,8 +1,31 @@
-#include <iostream>	
+#include <iostream>
 
+double LinearEquation(int a, int b)
+{
+	if (a == 0)
+	{
+		throw std::logic_error("divide by zero!");
+	}
 
+	return -((double)b / a);
+}
 
 int main()
 {
-	
+	int a;
+	int b;
+
+	std::cout << "두 정수를 입력하세요 : ";
+	std::cin >> a >> b;
+
+	try
+	{
+		std::cout << LinearEquation(a, b) << std::endl;
+	}
+	catch (std::logic_error)
+	{
+		std::cerr << "divide by zero!" << std::endl;
+	}
+
+	return 0;
 }
