d522fd6ab15215b008114b7096b7db44c77d5bf3
bigstar One <bigstarone96@gmail.com>
2023-02-17 22:55:51 +0900

    연습문제 - Stack

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,56 +1,106 @@
 #include <iostream>
-#include <chrono>
 
-using Comparison = bool(*)(int, int);
+const int STACK_SIZE{ 10 };
 
-// 오름차순
-bool Asscending(int x, int y)
+enum Command
 {
-	return x < y;
+	PUSH = 1,
+	POP = 2,
+	EXIT = 3
+};
+
+struct Stack
+{
+	int container[STACK_SIZE]{};
+	int topIndex = -1;
+};
+
+void PrintInfo()
+{
+	std::cout << "<STACK>" << std::endl;
+	std::cout << "[1] PUSH" << std::endl;
+	std::cout << "[2] POP" << std::endl;
+	std::cout << "[3] EIXT" << std::endl;
 }
 
-bool Descending(int x, int y)
+void PrintStack(Stack& stack)
 {
-	return x > y;
-}
+	std::cout << "----------" << std::endl;
 
-void Sort(int numbers[], int count, Comparison f)
-{
-	int temp{};
-
-	for (int i = 0; i < count; ++i)
+	for (int i = stack.topIndex; i > -1; --i)
 	{
-		for (int j = i + 1; j < count; ++j)
+		std::cout << i + 1 << ". " << stack.container[i] << std::endl;
+	}
+
+	std::cout << "----------" << std::endl;
+}
+
+void Push(Stack& stack)
+{
+	if (stack.topIndex == STACK_SIZE - 1)
+	{
+		std::cout << "더 이상 PUSH를 할 수 없습니다." << std::endl;
+		return;
+	}
+	int num{};
+	std::cout << "	> push value : ";
+	std::cin >> num;
+
+	stack.topIndex++;
+	stack.container[stack.topIndex] = num;
+}
+
+void Pop(Stack& stack)
+{
+	if (stack.topIndex == -1)
+	{
+		std::cout << "더 이상 POP을 할 수 없습니다." << std::endl;
+		return;
+	}
+	std::cout << stack.container[stack.topIndex] << " POP!" << std::endl;
+
+	stack.container[stack.topIndex] = NULL;
+	stack.topIndex--;
+}
+
+void ProcessUserInput(Stack& stack)
+{
+	int command = -1;
+
+	while (true)
+	{
+		PrintInfo();
+		PrintStack(stack);
+
+		std::cout << std::endl;
+		std::cout << "> ";
+		std::cin >> command;
+		switch (command)
 		{
-			if (f(numbers[i], numbers[j]))
-			{
-				temp = numbers[i];
-				numbers[i] = numbers[j];
-				numbers[j] = temp;
-			}
+			case PUSH:
+				Push(stack);
+				break;
+
+			case POP:
+				Pop(stack);
+				break;
+
+			case EXIT:
+				return;
+				break;
+
+			default:
+				std::cout << "잘못 입력하셨습니다." << std::endl;
+				break;
 		}
 	}
 }
 
 int main()
 {
-	const int NumArray{ 10 };
-	int scores[NumArray]{ 20,10,40,15,30,70,60,90,80,50 };
+	Stack myStack;
 
-	auto startTime = std::chrono::system_clock::now();
-	Sort(scores, NumArray, Asscending);
-	auto endTime = std::chrono::system_clock::now();
+	ProcessUserInput(myStack);
 
-	for (int i = 0; i < NumArray; ++i)
-	{
-		if (i == (NumArray - 1))
-		{
-			std::cout << scores[i] << std::endl;
-			break;
-		}
-		std::cout << scores[i] << ", ";
-	}
-	auto duration = endTime - startTime;
-
-	std::cout << "Sort() runs : " << duration.count() << "ms" << std::endl;
+	return 0;
 }
