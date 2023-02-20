552f492c2b2aa0ff335c8c41bfc8e1fbee0d92fd
bigstar One <bigstarone96@gmail.com>
2023-02-20 17:09:56 +0900

    LinkedList + 내가 만든 몬스터 만들기

---
+++ StudyMySelf2/MySelfCreatMonster.h
@@ -0,0 +1,34 @@
+#pragma once
+
+const int NAME_LENGTH{ 16 };
+
+struct MyMonster
+{
+	char name[NAME_LENGTH];
+	int hp;
+
+	MyMonster* pNext;
+};
+
+struct MyMonsterList
+{
+	MyMonster* pHead;
+	MyMonster* pTail;
+};
+
+enum Command
+{
+	Create = 1,
+	Delete = 2,
+	Exit = 3
+};
+
+void MySelfMonsterInfo();
+MyMonster* MySelfCreatMonster(MyMonsterList& list);
+void MySelfPrintMonster(const MyMonsterList& list);
+void HowManyCreatMonster(MyMonsterList& list);
+char* MySelfFindMonster(MyMonsterList& list);
+bool MySelfDeleteMonster(MyMonsterList& list);
+void MySelfDeleteAll(MyMonsterList& list);
+
+void MySelfMonsterProcessUserInput(MyMonsterList& list);
