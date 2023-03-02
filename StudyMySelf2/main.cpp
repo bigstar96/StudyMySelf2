ee52edb7da1d0c9b7fb71da782750eb44ee091df
bigstar One <bigstarone96@gmail.com>
2023-03-02 18:23:00 +0900

    class기초 - 10 (operator)

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -16,6 +16,31 @@ public:
 	}
 
 	friend Point2D operator+(const Point2D& x, const Point2D& y);
+	const Point2D& operator++();
+	const Point2D& operator++(int value);
+
+	operator const float()
+	{
+		return float(sqrt(mX * mX + mY * mY));
+	}
+
+	void operator()()
+	{
+		mX = 0;
+		mY = 0;
+	}
+
+	void operator()(int x, int y)
+	{
+		mX = x;
+		mY = y;
+	}
+
+	friend std::ostream& operator<< (std::ostream& os, const Point2D point)
+	{
+		os << "(" << point.mX << ", " << point.mY << ")";
+		return os;
+	}
 };
 
 Point2D operator+(const Point2D& x, const Point2D& y)
@@ -27,6 +52,63 @@ Point2D operator+(const Point2D& x, const Point2D& y)
 
 	return newPt;
 }
+
+// 전위 연산자
+const Point2D& Point2D::operator++()
+{
+	mX++;
+	mY++;
+
+	return *this;
+}
+
+// 후위 연산자
+const Point2D& Point2D::operator++(int)
+{
+	Point2D temp(mX, mY);
+	++(*this);
+	return temp;
+}
+
+
+class MyArray
+{
+private:
+	int mArray[10];
+
+public:
+	MyArray() : mArray{} {}
+
+	int& operator[](int index)
+	{
+		return mArray[index];
+	}
+};
+
+class Character
+{
+
+public:
+	Character& Move()
+	{
+		std::cout << "Move" << std::endl;
+		return *this;
+	}
+
+	Character& TurnLeft()
+	{
+		std::cout << "Turn Left" << std::endl;
+		return *this;
+	}
+
+	Character& TurnRight()
+	{
+		std::cout << "Turn Right" << std::endl;
+		return *this;
+	}
+
+};
+
 
 int main()
 {
@@ -41,5 +123,31 @@ int main()
 	pt4 = (pt1 + pt2) + pt3;
 	pt4.Print();
 
-}
+	Point2D pt5{ 2,3 };
+	++pt5;
+	pt5.Print();
 
+	MyArray ay1;
+	
+	ay1[0] = 1;
+
+	std::cout << ay1[0] << std::endl;
+	std::cout << ay1[1] << std::endl;
+
+	Point2D pt6{ 2,3 };
+	float dist{ pt6 };
+	std::cout << pt6 << std::endl;
+
+	Point2D pt7{ 2,3 };
+	pt7();
+	pt7.Print();
+	pt7(2, 3);
+	pt7.Print();
+
+	std::cout << pt1 << pt2 << pt3 << pt4 << pt5 << pt6 << pt7 << std::endl;
+
+	Character player;
+	player.Move()
+		.TurnLeft()
+		.TurnRight();
+}
