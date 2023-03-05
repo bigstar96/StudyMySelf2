44518e633c0351f71409c97b718603be65cf4149
bigstar One <bigstarone96@gmail.com>
2023-03-06 05:39:32 +0900

    프로그래밍이론 - 1

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
-#include "ClassString.h"
 
+class ClassA
+{
+public:
+	ClassA() {}
+};
+
+class ClassB
+{
+public:
+	ClassB() = default;
+};
 
 int main()
 {
-	ClassString empty;
-	std::cout << empty << std::endl;
+	std::cout << std::is_trivial<ClassA>::value << std::is_pod<ClassA>::value << std::endl;
 
-	ClassString sized(10);
-	std::cout << sized << std::endl;
-
-	ClassString s1("test");
-	std::cout << s1 << std::endl;
-
-	ClassString s2(s1);
-	std::cout << s2 << std::endl;
-
-	ClassString s3 = s1 + s2;
-	std::cout << s3 << std::endl;
-
-	s3 += s3;
-	std::cout << s3 << std::endl;
-
-	std::cout << s1[0] << s1[1] << s1[2] << s1[3] << s1[4] << std::endl;
-
-	// s3[0] = "T";
-
-	empty = s3;
-	std::cout << empty << std::endl;
-
-	empty = empty;
-	std::cout << empty << std::endl;
+	std::cout << std::is_trivial<ClassB>::value << std::is_pod<ClassB>::value << std::endl;
 }
