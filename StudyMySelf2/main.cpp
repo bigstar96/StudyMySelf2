9a6a03797458a32cdf6966c3295906083f29f3bd
bigstar One <bigstarone96@gmail.com>
2023-04-14 04:01:30 +0900

    18_1_Dynamic_Programming1 - 2

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,59 +1,82 @@
 #include <iostream>
+#include <map>
+#include <string>
 
-int Fibonacci(int n)
+using fib_history = std::map<int64_t, int64_t>;
+
+int64_t Fibonacci(int64_t n, fib_history& h)
 {
+	if (h.count(n) == 1)
+	{
+		return h[n];
+	}
+
 	if (n <= 2)
 	{
 		return 1;
 	}
 
-	return Fibonacci(n - 1) + Fibonacci(n - 2);
+	h[n] = Fibonacci(n - 1, h) + Fibonacci(n - 2, h);
+
+	return h[n];
 }
 
-int SimpleOne(int n)
+int FindWay(int m, int n)
 {
-	if (n <= 1)
+	if (m == 0 || n == 0)
+	{
+		return 0;
+	}
+
+	if (m == 1 && n == 1)
 	{
 		return 1;
 	}
 
-	SimpleOne(n - 1);
+	return FindWay(m - 1, n) + FindWay(m, n - 1);
 }
 
-int SimpleTwo(int n)
+using way_history = std::map<std::string, int>;
+
+int FindWays(int m, int n, way_history& h)
 {
-	if (n <= 1)
+	const std::string key = std::to_string(m) + "," + std::to_string(n);
+
+	if (h.count(key) == 1)
+	{
+		return h[key];
+	}
+
+	const std::string rkey = std::to_string(n) + "," + std::to_string(m);
+	if (h.count(rkey) == 1)
+	{
+		return h[rkey];
+	}
+
+	if (m == 0 || n == 0)
+	{
+		return 0;
+	}
+
+	if (m == 1 && n == 1)
 	{
 		return 1;
 	}
 
-	SimpleTwo(n - 2);
-}
+	h[key] = FindWays(m - 1, n, h) + FindWays(m, n - 1, h);
 
-int SimpleThree(int n)
-{
-	if (n <= 1)
-	{
-		return 1;
-	}
-
-	SimpleThree(n - 1);
-	SimpleThree(n - 1);
-}
-
-int SimpleFour(int n)
-{
-	if (n <= 1)
-	{
-		return 1;
-	}
-
-	SimpleFour(n - 2);
-	SimpleFour(n - 2);
+	return h[key];
 }
 
 int main()
 {
-	std::cout << Fibonacci(6) << std::endl;
-	std::cout << Fibonacci(40) << std::endl;
+	fib_history fh;
+	std::cout << Fibonacci(50, fh) << std::endl;
+
+	std::cout << FindWay(5, 5) << std::endl;
+
+	way_history wh;
+	std::cout << FindWays(20, 20, wh) << std::endl;
+
+	return 0;
 }
