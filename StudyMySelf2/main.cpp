4ebbc192637a9f3d39bbced8f92b71e4cd01dde7
bigstar One <bigstarone96@gmail.com>
2023-04-14 03:24:08 +0900

    17_3_Heap

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,52 +1,125 @@
 #include <iostream>
-#include "QuadTree.h"
-#include "binarytree.h"
+#include <vector>
+#include <algorithm>
+
+class Heap
+{
+	std::vector<int> mHeap;
+
+public:
+	void Insert(int value)
+	{
+		mHeap.push_back(value);
+
+		unsigned long i = mHeap.size() - 1;
+		unsigned long half = (i - 1) / 2;
+
+		while (i != 0 && value > mHeap[half])
+		{
+			mHeap[i] = mHeap[half];
+			mHeap[half] = value;
+			i = half;
+
+			half = (i - 1) / 2;
+		}
+	}
+
+	int Pop()
+	{
+		if (mHeap.size() == 0)
+		{
+			return 0;
+		}
+
+		int item = mHeap.front();
+		mHeap[0] = mHeap.back();
+		mHeap.pop_back();
+
+		int i{ 0 };
+
+		while (i < mHeap.size())
+		{
+			unsigned long big_child = (i * 2) + 1;
+
+			if (big_child >= mHeap.size())
+			{
+				break;
+			}
+
+			if (big_child + 1 < mHeap.size() &&
+				mHeap[big_child] < mHeap[big_child + 1])
+			{
+				big_child++;
+			}
+
+			if (mHeap[i] >= mHeap[big_child])
+			{
+				break;
+			}
+
+			std::swap(mHeap[i], mHeap[big_child]);
+
+			i = big_child;
+		}
+		return item;
+	}
+
+	void Print()
+	{
+		for (auto& e : mHeap)
+		{
+			std::cout << e << " ";
+		}
+	}
+};
 
 
 int main()
 {
-	/*QuadTree center1(treePoint(0, 0), treePoint(8, 8));
+	Heap h;
 
-	std::unique_ptr<Node> a = std::make_unique<Node>(treePoint(1, 1), 1);
-	std::unique_ptr<Node> b = std::make_unique<Node>(treePoint(2, 2), 2);
-	std::unique_ptr<Node> c = std::make_unique<Node>(treePoint(3, 3), 3);
-	std::unique_ptr<Node> d = std::make_unique<Node>(treePoint(4, 4), 4);
+	h.Insert(1);
+	h.Insert(2);
+	h.Insert(3);
+	h.Insert(4);
+	h.Insert(5);
 
-	center1.Insert(a.get());
-	center1.Insert(b.get());
-	center1.Insert(c.get());
-	center1.Insert(d.get());
+	std::cout << std::endl << "---------------" << std::endl;
+	std::cout << h.Pop();
+	std::cout << h.Pop();
+	std::cout << h.Pop();
+	std::cout << h.Pop();
+	std::cout << h.Pop();
 
-	std::cout << "Node b:" << center1.Search(treePoint(2, 2))->data;
+	h.Print();
 
-	return 0;*/
-
-	mytree::Binarytree myTree;
-
-	auto pRoot = myTree.GetRoot();
-	pRoot->mData = 1;
-
-	auto pNode = myTree.InsertLeft(pRoot, 2);
-	myTree.InsertLeft(pNode, 4);
-	myTree.InsertRight(pNode, 5);
-	pNode = myTree.InsertRight(pRoot, 3);
-	myTree.InsertLeft(pNode, 6);
-	myTree.InsertRight(pNode, 7);
-
-	std::cout << "---In Order---" << std::endl;
-	myTree.InOrder(pRoot);
-	std::cout << std::endl << "---Pre Order---" << std::endl;
-	myTree.PreOrder(pRoot);
-	std::cout << std::endl << "---Post Order---" << std::endl;
-	myTree.PostOrder(pRoot);
+	std::cout << std::endl;
 	std::cout << std::endl;
 
+	std::vector<int> v{ 1,2,3,4,5 };
 
-	myTree.BreadthFirst();
-	std::cout << std::endl;
-	myTree.DepthFirst();
-	std::cout << std::endl;
+	// Max Heap
+	std::make_heap(v.begin(), v.end());
+	for (auto& e : v)
+	{
+		std::cout << e << std::endl;
+	}
+	std::cout << "---------------" << std::endl;
 
-	std::cout << myTree.Search(pRoot, 6) << std::endl;
-	std::cout << myTree.Sum(pRoot) << std::endl;
+	// 삭제 및 출력( 정렬 )
+	while (!v.empty())
+	{
+		std::cout << v.front() << std::endl;
+		std::pop_heap(v.begin(), v.end());
+		v.pop_back();
+	}
+	std::cout << "---------------" << std::endl;
+	
+	// Min Heap
+	v = { 4,2,5,1,3 };
+	std::make_heap(v.begin(), v.end(), std::greater<>());
+	for (auto& e : v)
+	{
+		std::cout << e << std::endl;
+	}
 }
