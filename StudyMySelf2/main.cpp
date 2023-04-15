6df551d8ea60dcd7853886d7361d6a1dc9edc69c
bigstar One <bigstarone96@gmail.com>
2023-04-16 03:01:45 +0900

    18_1_Dynamic_Programming1 - 3

--- StudyMySelf2/main.cpp
+++ StudyMySelf2/main.cpp
@@ -1,82 +1,52 @@
 #include <iostream>
+#include <vector>
 #include <map>
-#include <string>
 
-using fib_history = std::map<int64_t, int64_t>;
+using accum_history = std::map<int, bool>;
 
-int64_t Fibonacci(int64_t n, fib_history& h)
+bool CanAccumulate(int sum, const std::vector<int>& numbers, accum_history& h)
 {
-	if (h.count(n) == 1)
+	if (h.count(sum) == 1)
 	{
-		return h[n];
+		return h[sum];
+	}
+	if (sum == 0)
+	{
+		return true;
+	}
+	if (sum < 0)
+	{
+		return false;
 	}
 
-	if (n <= 2)
+	int remain{};
+
+	for (auto e : numbers)
 	{
-		return 1;
+		remain = sum - e;
+		if (CanAccumulate(remain, numbers, h) == true)
+		{
+			h[sum] = true;
+			return true;
+		}
 	}
-
-	h[n] = Fibonacci(n - 1, h) + Fibonacci(n - 2, h);
-
-	return h[n];
+	h[sum] = false;
+	return false;
 }
 
-int FindWay(int m, int n)
-{
-	if (m == 0 || n == 0)
-	{
-		return 0;
-	}
 
-	if (m == 1 && n == 1)
-	{
-		return 1;
-	}
-
-	return FindWay(m - 1, n) + FindWay(m, n - 1);
-}
-
-using way_history = std::map<std::string, int>;
-
-int FindWays(int m, int n, way_history& h)
-{
-	const std::string key = std::to_string(m) + "," + std::to_string(n);
-
-	if (h.count(key) == 1)
-	{
-		return h[key];
-	}
-
-	const std::string rkey = std::to_string(n) + "," + std::to_string(m);
-	if (h.count(rkey) == 1)
-	{
-		return h[rkey];
-	}
-
-	if (m == 0 || n == 0)
-	{
-		return 0;
-	}
-
-	if (m == 1 && n == 1)
-	{
-		return 1;
-	}
-
-	h[key] = FindWays(m - 1, n, h) + FindWays(m, n - 1, h);
-
-	return h[key];
-}
 
 int main()
 {
-	fib_history fh;
-	std::cout << Fibonacci(50, fh) << std::endl;
+	accum_history h;
 
-	std::cout << FindWay(5, 5) << std::endl;
+	std::cout << CanAccumulate(900, { 7, 14 }, h) << std::endl;
 
-	way_history wh;
-	std::cout << FindWays(20, 20, wh) << std::endl;
+	h.clear();
+	std::cout << CanAccumulate(7, { 5,3,4,7 }, h) << std::endl;
+
+	h.clear();
+	std::cout << CanAccumulate(7, { 2,4 }, h) << std::endl;
 
 	return 0;
 }
